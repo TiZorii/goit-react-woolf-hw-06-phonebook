93e938ffc418895acb06780015d426185eb72c84
TiZorii <131365289+TiZorii@users.noreply.github.com>
2024-03-16 10:14:41 -0400

    Update slice.js

--- src/redux/contacts/slice.js
+++ src/redux/contacts/slice.js
@@ -5,10 +5,10 @@ import { nanoid } from 'nanoid';
 
 const initialState = {
   list: [
-    { id: 'id-1', name: 'Rosie Simpson', tel: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', tel: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', tel: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', tel: '227-91-26' },
+    { id: 'id-1', name: 'Harper Rodriguez', tel: '459-12-56' },
+    { id: 'id-2', name: 'Mia Chen', tel: '443-89-12' },
+    { id: 'id-3', name: 'Donovan Patel', tel: '645-17-79' },
+    { id: 'id-4', name: 'Isabella Santiago', tel: '227-91-26' },
   ],
 };
 
